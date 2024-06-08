771b4c1e691836ab452dba049b345852532fd343
Mgll3 <miguel.alvarez1@udea.edu.co>
2024-06-08 15:37:57 -0500

    Transactions

---
+++ utils/queries/transactions.ts
@@ -0,0 +1,23 @@
+import { gql } from '@apollo/client';
+
+
+const GET_TRANSACTION_BY_ID = gql`
+    query Query($take: Int, $skip: Int, $where: TransactionWhereInput) {
+        transactions(take: $take, skip: $skip, where: $where) {
+        product {
+            title
+            id
+            balance
+        }
+        user {
+            name
+        }
+        createdAt
+        id
+        type
+        amount
+        }
+    }
+`;
+
+export {GET_TRANSACTION_BY_ID };
--- utils/queries/users.ts
+++ utils/queries/users.ts
@@ -8,6 +8,7 @@ const GET_USERS = gql`
     image
     email
     createdAt
+    role
   }
 }
 
